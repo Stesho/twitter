d96688b864a14da0918f68695affe91965a178fa
Steshenko Dmitry <3301297@gmail.com>
2023-12-21 09:10:25 +0300

    feat: add Input component

--- src/components/ui/Input/Input.styled.ts
+++ src/components/ui/Input/Input.styled.ts
@@ -2,10 +2,16 @@ import styled from 'styled-components';
 
 export const InputWrapper = styled.input`
   width: 450px;
-  height: 70px;
+  padding: 23px 20px 26px 20px;
   flex-shrink: 0;
   border-radius: 6px;
   border: 1px solid rgba(0, 0, 0, 0.2);
-
   background: #fff;
+  color: rgba(0, 0, 0, 0.6);
+  font-family: Roboto, sans-serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  outline: none;
 `;
