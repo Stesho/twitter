25764a4bdf634c0feec1ee411a7dca43ea9f754a
Steshenko Dmitry <3301297@gmail.com>
2023-12-22 16:54:41 +0300

    feat: add Tweet component

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ import { ButtonTypes } from "@/types/buttonTypes";
 import { Switch } from "@/components/ui/Switch/Switch";
 import { Select } from "@/components/ui/Select/Select";
 import { Menu } from "@/components/ui/Menu/Menu";
+import Tweet from "@/components/ui/Tweet/Tweet";
+
+const tweet = {
+  name: "Bobur",
+  username: "@bobur_mavlonov",
+  text: "4-kursni tugatgunimcha kamida bitta biznesim bo'lishini, uylanish uchun moddiy jihatdan to'la-to'kis tayyor bo'lishni, sog'lik va jismoniy holatni normallashtirishni reja qildim",
+  date: "Apr 1",
+};
 
 export const App = () => (
   <div>
+    <Tweet
+      username={tweet.username}
+      name={tweet.name}
+      text={tweet.text}
+      date={tweet.date}
+    />
     <Input />
     <Button disabled>Tweet</Button>
     <Button type={ButtonTypes.Primary}>Tweet</Button>
