a1741a8b4714d9c8db4f7640f80879ad4089b9c0
Steshenko Dmitry <3301297@gmail.com>
2023-12-28 00:52:41 +0300

    feat: connect redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,41 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 import userReducer from '@/store/slices/userSlice';
 import tweetsReducer from '@/store/slices/tweetsSlice';
+
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
   tweets: tweetsReducer,
 });
 
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
