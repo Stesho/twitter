e887005990c9f99b87e3c5d7e95f8e687696a3be
Steshenko Dmitry <3301297@gmail.com>
2023-12-31 01:13:07 +0300

    fix: move firebase keys to env variables

--- src/utils/getDaysInMonth.ts
+++ src/utils/getDaysInMonth.ts
@@ -1,5 +1,4 @@
 export const getDaysInMonth = (year: number, month: number) => {
   const daysCount = new Date(year, month, 0).getDate();
-  console.log(year, month, daysCount);
   return [...new Array(daysCount)].map((_, index) => (index + 1).toString());
 };
